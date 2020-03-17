a17cb796cec7ce09ca5c0d8289c15e16e7c195e2
Jeffery Gontarek <jeffery@jeffsmac.local>
2020-03-16 19:07:48 -0700

    initial commit

---
+++ app/views/cheeses/_cheese.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! cheese, :id, :name, :age, :created_at, :updated_at
+json.url cheese_url(cheese, format: :json)
---
+++ test/system/cheeses_test.rb
@@ -0,0 +1,45 @@
+require "application_system_test_case"
+
+class CheesesTest < ApplicationSystemTestCase
+  setup do
+    @cheese = cheeses(:one)
+  end
+
+  test "visiting the index" do
+    visit cheeses_url
+    assert_selector "h1", text: "Cheeses"
+  end
+
+  test "creating a Cheese" do
+    visit cheeses_url
+    click_on "New Cheese"
+
+    fill_in "Age", with: @cheese.age
+    fill_in "Name", with: @cheese.name
+    click_on "Create Cheese"
+
+    assert_text "Cheese was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Cheese" do
+    visit cheeses_url
+    click_on "Edit", match: :first
+
+    fill_in "Age", with: @cheese.age
+    fill_in "Name", with: @cheese.name
+    click_on "Update Cheese"
+
+    assert_text "Cheese was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Cheese" do
+    visit cheeses_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Cheese was successfully destroyed"
+  end
+end
